6f423408f4ea4c6ace3a935ed0ff63756129025c
Yorman <yjona@espe.edu.ec>
2024-06-16 21:41:09 -0500

    Version New

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,13 @@ import { ProductListComponent } from './product-list/product-list.component';
 import { ProductAddComponent } from './product-add/product-add.component';
 import { ProductEditComponent } from './product-edit/product-edit.component';
 import { ProductDeleteComponent } from './product-delete/product-delete.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 export const routes: Routes = [
   { path: 'product-list', component: ProductListComponent },
   { path: 'product-add', component: ProductAddComponent },
   { path: 'product-edit/:id', component: ProductEditComponent },
   { path: 'product-delete/:id', component: ProductDeleteComponent },
-  { path: '', redirectTo: '/product-list', pathMatch: 'full' }
+  { path: '', redirectTo: '/product-list', pathMatch: 'full' },
+  { path: '**', component: PageNotFoundComponent }
 ];
